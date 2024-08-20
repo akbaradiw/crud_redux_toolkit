a91487bb982127bfd42d3ece5fdf48e81454e6c2
akbaradiw <akbaradiw000@gmail.com>
2024-08-20 23:18:24 +0700

    commit branch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import FormComp from '../component/FormComp'
 import Navbar from '../component/Navbar'
+import Note from '../component/Note'
 const Home = () => {
 
     return (
         <div>
             <Navbar />
-            <FormComp />
+            {/* <FormComp /> */}
+            <Note />
         </div>
     )
 }
